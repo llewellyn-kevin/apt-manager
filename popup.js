8cab27ebb558659e0fab446163374ea223e694e6
Connor Lindsey <cblindsey3@gmail.com>
2018-11-02 11:13:00 -0600

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    const settings = document.getElementById('settings');
+
+    if (settings) {
+        settings.addEventListener('click', e => {
+            chrome.tabs.create({ url: "/settings.html" });
+            alert('something worked');
+        });
+
+    }
+    
+}
